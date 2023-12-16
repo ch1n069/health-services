0a31ed16fd6fceaa6ed4f26f13c681e0b4c3a783
Bruno Alpachino <brunolalpachino@gmail.com>
2023-12-16 09:37:48 +0300

    styling of the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Hero from "./components/Hero";
+import NavBar from "./components/NavBar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
+      <NavBar />
       <Hero />
     </div>
   );
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { IoMdMenu } from "react-icons/io";
+const NavBar = () => {
+  const [nav, setNav] = useState(false);
+  const handleToggleNav = () => {
+    setNav(!nav);
+  };
+  return (
+    <div className="absolute w-full flex justify-between p-4 items-center">
+      <h1 className="text-white font-semibold text-2xl z-20"> Health Care</h1>
+      <IoMdMenu
+        onClick={handleToggleNav}
+        className="z-20 text-white cursor-pointer"
+        size={25}
+      />
+      <div
+        className={
+          nav
+            ? " ease-in duration-300 fixed text-gray-300 left-0 top-0 w-full h-screen bg-black/90 px-4 py-7 flex flex-col z-10"
+            : "absolute top-0 h-screen left-[-100%] ease-in duration-500 z-10"
+        }
+      >
+        <ul className="flex flex-col fixed w-full h-full items-center justify-center">
+          <li className="font-bold text-2xl p-5">Home</li>
+          <li className="font-bold text-2xl p-5">Reservations</li>
+          <li className="font-bold text-2xl p-5">Rooms</li>
+          <li className="font-bold text-2xl p-5">Amenities</li>
+          <li className="font-bold text-2xl p-5">Staff</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
