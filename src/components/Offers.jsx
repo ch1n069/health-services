5b9cd055d7c147426efa170bc8402007a54df109
Bruno Alpachino <brunolalpachino@gmail.com>
2023-12-16 12:28:58 +0300

    created and offers component

---
+++ src/components/Offers.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Offers = () => {
+  return (
+    <div className="max-w-[900px] m-auto px-4  py-12 flex flex-wrap justify-between">
+      <p className="text-large font-bold text-gray-700">
+        Health care insentives
+      </p>
+      <p className="text-large font-bold text-gray-700">
+        Health care insentives
+      </p>
+
+      <p className="text-large font-bold text-gray-700">Competitive rate </p>
+
+      <p className="text-large font-bold text-gray-700">Book with</p>
+    </div>
+  );
+};
+
+export default Offers;
